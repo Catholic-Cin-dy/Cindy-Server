cfea20292cae54fae63cd1266edb4e83f4b18841
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-03-28 00:33:04 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,6 +31,9 @@ DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo ">>> DEPLOY_JAR 위치" $DEPLOY_JAR    >> /var/www/html/deploy.log
 echo ">>> DEPLOY_JAR 배포"    >> /var/www/html/deploy.log
 chmod -R 777 $DEPLOY_JAR
-echo ">>>DEPLOY_JAR 권한 부여"
+echo ">>>DEPLOY_JAR 권한 부여" >> /var/www/html/deploy.log
 
 nohup java -jar -Dspring.profiles.active=local $DEPLOY_JAR >> /var/www/html/deploy.log 2>/var/www/html/deploy_err.log &
+
+
+echo ">>>배포완료" >> /var/www/html/deploy.log
