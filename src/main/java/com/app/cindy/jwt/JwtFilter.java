fc25e57a309a08b72a0e6762cc9483924c38fcc9
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-03-28 15:19:36 +0900

    refactor : Security 파일 수정

--- src/main/java/com/app/cindy/jwt/JwtFilter.java
+++ src/main/java/com/app/cindy/jwt/JwtFilter.java
@@ -18,7 +18,7 @@ public class JwtFilter extends GenericFilterBean {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
 
-    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String AUTHORIZATION_HEADER = "X-AUTH-TOKEN";
 
     private TokenProvider tokenProvider;
 
@@ -36,22 +36,14 @@ public class JwtFilter extends GenericFilterBean {
         if (StringUtils.hasText(jwt)&& tokenProvider.validateToken(servletRequest,jwt)) {
             Authentication authentication = tokenProvider.getAuthentication(jwt);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-            logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+            logger.info("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
 
         } else {
-            logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+            logger.info("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
         }
 
 
         filterChain.doFilter(servletRequest, servletResponse);
-    }
-
-    private String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return bearerToken;
     }
 
 }
