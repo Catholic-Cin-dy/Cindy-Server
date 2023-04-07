ac732bf40570b028382b8f079f8ac05f3449d52a
mac <oversteam@naver.com>
2023-04-07 19:47:52 +0900

    Feat : ootd 게시글 전체 조회(완성)

--- src/main/java/com/app/cindy/controller/BoardController.java
+++ src/main/java/com/app/cindy/controller/BoardController.java
@@ -4,16 +4,14 @@ import com.app.cindy.common.CommonResponse;
 import com.app.cindy.domain.user.User;
 import com.app.cindy.dto.PageResponse;
 import com.app.cindy.dto.board.BoardRes;
+import com.app.cindy.dto.user.UserReq;
 import com.app.cindy.service.BoardService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Min;
 import java.util.List;
@@ -26,13 +24,14 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    @GetMapping("")
-    @ApiOperation(value = "04-01 ootd 게시판 조회 👗 API Response #FRAME PRODUCT 01", notes = "")
+    @PostMapping("")
+    @ApiOperation(value = "04-01 ootd 게시판 조회 👗", notes = "")
     public CommonResponse<PageResponse<List<BoardRes.BoardList>>> getBoardList(@AuthenticationPrincipal User user,
                                                                                @Parameter(description = "페이지", example = "0") @RequestParam(required = false,defaultValue = "0" ) @Min(value = 0) Integer page,
                                                                                @Parameter(description = "페이지 사이즈", example = "10") @RequestParam(required = false,defaultValue = "10")  Integer size,
-                                                                               @RequestParam(value = "정렬",required = false,defaultValue="0") Integer sort){
-        PageResponse<List<BoardRes.BoardList>> boardList = boardService.getBoardList(sort,page,size,user.getId());
+                                                                               @RequestParam(value = "정렬",required = false,defaultValue="0") Integer sort,
+                                                                               @RequestBody(required = false) UserReq.Distance distance){
+        PageResponse<List<BoardRes.BoardList>> boardList = boardService.getBoardList(sort,page,size,user.getId(),distance);
         System.out.println(boardList);
 
         return  CommonResponse.onSuccess(boardList);
